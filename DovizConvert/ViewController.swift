d3ec781fd50146984ac8649fbf7caf1a4d980301
Bircan Sezgin <bircansezgin@outlook.com.tr>
2023-06-11 19:20:01 +0300

    Duzeltiyorum

--- DovizConvert/ViewController.swift
+++ DovizConvert/ViewController.swift
@@ -10,25 +10,76 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    
+    
     //http://data.fixer.io/api/latest?access_key=65c9f5e1913452a764636d551a167c89
     
     // 3 step.
     
     @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var euroLabel: UILabel!
+    
     @IBOutlet weak var gbpLabel: UILabel!
+
+    
     @IBOutlet weak var jpyLabel: UILabel!
+    
     @IBOutlet weak var usdLabel: UILabel!
+
+    
     @IBOutlet weak var tryLabel: UILabel!
+        var tryLabelDeger: Double?
+        
+    var noOpTry = 0.0
+    var noOpGPB = 0.0
+    var noOpEuro = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startApi()
+    }//Finish viewController
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "go2"{
+            let des = segue.destination as! ViewController2
+            if let senderDeger = sender as? Double{
+                des.secondSendMoney = senderDeger
+            }
+        }
     }
     
+
     @IBAction func getRatesButton(_ sender: Any) {
         
+        let alerts = UIAlertController(title: "USD ", message: "ile Islem", preferredStyle: .alert)
+        let trybutton = UIAlertAction(title: "TRY", style: .default) { UIAlertAction in
+            self.performSegue(withIdentifier: "go2", sender: self.noOpTry)
+        }
+        let gbpButton = UIAlertAction(title: "GBP", style: .default) { UIAlertAction in
+            self.performSegue(withIdentifier: "go2", sender: self.noOpGPB)
+        }
+        
+        let euroButton = UIAlertAction(title: "EURO", style: .default) { UIAlertAction in
+            self.performSegue(withIdentifier: "go2", sender: self.noOpEuro)
+        }
+        
+        alerts.addAction(trybutton)
+        alerts.addAction(gbpButton)
+        alerts.addAction(euroButton)
+        self.present(alerts, animated: true)
+        
+   
+
+    }
+    
+    
+    
+    
+    func startApi(){
         // 1) Request & Session (Istek Yollamak)
-        let url = URL(string: "http://data.fixer.io/api/latest?access_key=65c9f5e1913452a764636d551a167c89")
+
+        let url = URL(string: "https://api.fastforex.io/fetch-all?api_key=6431095421-46814537d2-rw2cuf")
+        //"http://data.fixer.io/api/latest?access_key=65c9f5e1913452a764636d551a167c89"
         
         // Session
         let session = URLSession.shared
@@ -36,12 +87,8 @@ class ViewController: UIViewController {
         
         let task = session.dataTask(with: url!) { data, response, error in
             if error != nil{
-                let alert = UIAlertController(title: "Hata", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
-                let ok = UIAlertAction(title: "OK", style: .default)
-                alert.addAction(ok)
-                self.present(alert, animated: true)
-            }else{
                 
+            }else{
                 // 2) Response & Data (IStek Almak)
                 if data != nil{
                     do{
@@ -49,13 +96,15 @@ class ViewController: UIViewController {
                         let jsonResponse = try JSONSerialization.jsonObject(with: data! ,options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
                         //ASYNC
                         DispatchQueue.main.async {
-                            if let rates = jsonResponse["rates"] as? [String : Any]{
-                                //print(rates)
+                            if let rates = jsonResponse["results"] as? [String : Any]{
+                                
+                                
                                 if let cad = rates["CAD"] as? Double{
                                     self.cadLabel.text = "CAD: \(cad)"
                                 }
                                 if let gbp = rates["GBP"] as? Double{
                                     self.gbpLabel.text = "GPB: \(gbp)"
+                                    self.noOpGPB = gbp
                                 }
                                 if let jpy = rates["JPY"] as? Double{
                                     self.jpyLabel.text = "JPY: \(jpy)"
@@ -65,10 +114,19 @@ class ViewController: UIViewController {
                                 }
                                 if let trY = rates["TRY"] as? Double{
                                     self.tryLabel.text = "TRY: \(trY)"
+                                    self.noOpTry = trY
                                 }
-                            }
+                                if let eurO = rates["EUR"] as? Double{
+                                    self.euroLabel.text = "EURO: \(eurO)"
+                                    self.noOpEuro = eurO
+                                }
+                                
+                            
+                                
+                            }// Finish Rates!
+                            
                         }
-                        
+                       
                     }catch{
                         print("Error")
                     }
@@ -78,15 +136,7 @@ class ViewController: UIViewController {
         
         task.resume()
         
-        
-       
-        
-        
-        
-        
     }
-    
-    
     
 }
 
--- DovizConvert/ViewController2.swift
+++ DovizConvert/ViewController2.swift
@@ -9,21 +9,76 @@ import UIKit
 
 class ViewController2: UIViewController {
 
+    @IBOutlet weak var baseCurrentLabel: UILabel!
+    @IBOutlet weak var secondCurrentLabel: UILabel!
+    @IBOutlet weak var amountTextField: UITextField!
+    
+    var secondSendMoney = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print(secondSendMoney)
+        secondCurrentLabel.text = "\(secondSendMoney)"
+        
+        startApi()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func calculateButtonClick(_ sender: Any) {
+    
+        if let secondMoney = Double(secondCurrentLabel.text ?? ""), let inputMoney = Double(amountTextField.text ?? "") {
+              let toplam = secondMoney * inputMoney
+              let formattedToplam = String(format: "%.2f", toplam)
+              let formattedSecondMoney = String(format: "%.2f", secondMoney)
+              let formattedInputMoney = String(format: "%.2f", inputMoney)
+              
+              let alerts = UIAlertController(title: "Exchange", message: "Result : \(formattedSecondMoney) * \(formattedInputMoney) = \(formattedToplam)", preferredStyle: .alert)
+              let ok = UIAlertAction(title: "Ok", style: .cancel)
+              alerts.addAction(ok)
+              self.present(alerts, animated: true)
+          }
     }
-    */
+    
+    
+    
+    
+    func startApi(){
+        // 1) Request & Session (Istek Yollamak)
 
+        let url = URL(string: "https://api.fastforex.io/fetch-all?api_key=6431095421-46814537d2-rw2cuf")
+        //"http://data.fixer.io/api/latest?access_key=65c9f5e1913452a764636d551a167c89"
+        
+        // Session
+        let session = URLSession.shared
+        
+        
+        let task = session.dataTask(with: url!) { data, response, error in
+            if error != nil{
+                
+            }else{
+                // 2) Response & Data (IStek Almak)
+                if data != nil{
+                    do{
+                        // 3) Parsing & JSON Serialization
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data! ,options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
+                        //ASYNC
+                        DispatchQueue.main.async {
+                            if let rates = jsonResponse["results"] as? [String : Any]{
+                                print(rates)
+                                if let usd = rates["USD"] as? Double{
+                                    self.baseCurrentLabel.text = "USD: \(usd)"
+                                }
+                            }
+                        }
+                       
+                    }catch{
+                        print("Error")
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+        
+    }
+    
 }
