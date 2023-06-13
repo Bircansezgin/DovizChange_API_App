13e858a09fa73640cbfe4e0d7a13dc70f559ca63
Bircan Sezgin <bircansezgin@outlook.com.tr>
2023-06-13 03:24:46 +0300

    Siniflara tanimalalar Yapildi!

---
+++ DovizConvert/cellViewController.swift
@@ -0,0 +1,23 @@
+//
+//  cellViewController.swift
+//  DovizConvert
+//
+//  Created by Bircan Sezgin on 13.06.2023.
+//
+
+import UIKit
+
+class cellViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
